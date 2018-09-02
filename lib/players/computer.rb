bf066a2263d09591cf64491668e25d7de2f54f0f
Austin Powell <austinpowell58@gmail.com>
2018-09-02 21:32:25 +0000

    Done.

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -3,8 +3,14 @@ module Players
   class Computer < Player
     
     def valid_moves
-      counter 
-      @board.cells.select do 
+      array = []
+      @board.cells.each do |value|
+        if value == " "
+          array << @board.cells.index[value]
+        end 
+      end 
+      binding.pry
+    end
         
     
     def move(input)
