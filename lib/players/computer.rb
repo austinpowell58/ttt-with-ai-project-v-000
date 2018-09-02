6d3eb314ef2d08abe7acf6c07df1a78dad482866
Austin Powell <austinpowell58@gmail.com>
2018-09-02 21:24:53 +0000

    Done.

---
+++ lib/players/computer.rb
@@ -0,0 +1,15 @@
+require "pry"
+module Players
+  class Computer < Player
+    
+    def valid_moves
+      counter 
+      @board.cells.select do 
+        
+    
+    def move(input)
+      @board = input
+    end 
+    
+  end 
+end
