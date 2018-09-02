536164b600e810a81a3d3c0f159c97c53155d9b7
Austin Powell <austinpowell58@gmail.com>
2018-09-02 22:31:23 +0000

    Done.

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -3,13 +3,20 @@ module Players
 
   class Computer < Player
     
+    def corner
+      ["1", "3","7", "9"].find{|a| @board.taken?(a)} 
+    end
+    
+      
   
         
     def move(input)
       @board = input
       answer = [*1..9].sample
-      if !@board.taken?(answer)
-        answer.to_s
+      if !@board.taken?("5")
+        "5"
+      elsif corner
+        corner
       else
         answer = [*1..9].sample
       end
